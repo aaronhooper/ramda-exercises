d08bf6ee0220bfbfaf54eeacfcaf6661c362a34a
Aaron Hooper <rnjckhpr@gmail.com>
2023-04-10 15:54:27 +0100

    Add vowelRemover solution

---
+++ test/vowelRemover.test.js
@@ -0,0 +1,16 @@
+const strictEqual = require('chai').assert.strictEqual;
+const shortcut = require('../lib/vowelRemover')
+
+function doTest (input, expected) {
+  const actual = shortcut(input);
+  strictEqual(actual, expected, `for "${input}":\n`);
+}
+
+describe("Sample Tests", () => {
+  it("Should pass sample tests", () => {
+    doTest('hello', 'hll');
+    doTest('how are you today?', 'hw r y tdy?');
+    doTest('complain', 'cmpln');
+    doTest('never', 'nvr');
+  });    
+});
