a1c18b8f858e5e09d0668b75d099e587a1fa34b3
Aaron Hooper <rnjckhpr@gmail.com>
2023-04-18 18:31:37 +0100

    Lint

--- src/lib/smallEnough.js
+++ src/lib/smallEnough.js
@@ -4,7 +4,7 @@ const { all } = require('ramda')
  * You will be given an array and a limit value. You must check that all
  * values in the array are below or equal to the limit value. If they
  * are, return true. Else, return false.
- * 
+ *
  * You can assume all values in the array are numbers.
  */
 const smallEnough = (a, limit) => all((n) => n <= limit, a)
