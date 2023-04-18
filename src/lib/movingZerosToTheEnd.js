6685ce2e153a55a851c5c3f9a227d4aa4fcf4eee
Aaron Hooper <rnjckhpr@gmail.com>
2023-04-18 18:26:47 +0100

    Add descriptions to files

--- src/lib/movingZerosToTheEnd.js
+++ src/lib/movingZerosToTheEnd.js
@@ -1,5 +1,11 @@
 const { converge, concat, filter, reject, equals } = require('ramda')
 
+/**
+ * Write an algorithm that takes an array and moves all of the zeros to
+ * the end, preserving the order of the other elements.
+ *
+ * moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns [false,1,1,2,1,3,"a",0,0]
+*/
 const moveZeros = converge(concat, [reject(equals(0)), filter(equals(0))])
 
 module.exports = moveZeros
--- src/lib/vowelRemover.js
+++ src/lib/vowelRemover.js
@@ -1,3 +1,14 @@
+/**
+ * Create a function called shortcut to remove the lowercase vowels
+ * (a, e, i, o, u) in a given string.
+ *
+ * Examples:
+ *
+ * "hello"     -->  "hll"
+ * "codewars"  -->  "cdwrs"
+ * "goodbye"   -->  "gdby"
+ * "HELLO"     -->  "HELLO"
+ */
 const shortcut = (str) => str.replace(/[aeiou]/g, '')
 
 module.exports = shortcut
