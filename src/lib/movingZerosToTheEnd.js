35678ea59f0adecf3860b63cec3f53e71de1e1eb
Aaron Hooper <rnjckhpr@gmail.com>
2023-04-19 09:54:03 +0100

    Wrap moveZeros in function declaration

--- src/lib/movingZerosToTheEnd.js
+++ src/lib/movingZerosToTheEnd.js
@@ -4,7 +4,7 @@ const { converge, concat, filter, reject, equals } = require('ramda')
  * Write an algorithm that takes an array and moves all of the zeros to
  * the end, preserving the order of the other elements.
  *
- * @param {array} a
+ * @param {array} arr
  * array of any type
  * @returns {array} array with zeroes moved to the end
  *
@@ -12,6 +12,11 @@ const { converge, concat, filter, reject, equals } = require('ramda')
  * // returns [false,1,1,2,1,3,"a",0,0]
  * moveZeros([false,1,0,1,2,0,1,3,"a"])
  */
-const moveZeros = converge(concat, [reject(equals(0)), filter(equals(0))])
+function moveZeros (arr) {
+  return converge(concat, [
+    reject(equals(0)),
+    filter(equals(0))
+  ])(arr)
+}
 
 module.exports = moveZeros
